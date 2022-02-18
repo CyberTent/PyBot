b4b315962d21e59306d0a9a39d1aa546ee0393c5
Nikita Yarosh <yarosh.nv@yandex.ru>
2022-02-18 17:38:42 +0300

    Add files via upload

--- server_info/info.py
+++ server_info/info.py
@@ -15,7 +15,8 @@ class ServerInfo(commands.Cog):
         for server_name in self.bot.guilds:
             print(f'{server_number} | {server_name} | {server_name.id} | {len(server_name.members)}')
             for member in server_name.members:
-                print(f'\t{member_count} || {member.nick} || {member.name} || {member.discriminator} || {member.id} || {member.bot}')
+                print(f'\t{member_count} || {member.nick} || {member.name} || '
+                      f'{member.discriminator} || {member.id} || {member.bot}')
                 member_count += 1
             member_count = 1
             server_number += 1
@@ -26,12 +27,18 @@ class ServerInfo(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        id = self.bot.get_channel(id=int(message.channel.id))
+        channel_id = self.bot.get_channel(id=int(message.channel.id))
         channel = self.bot.get_channel(id=943519031418294292)
-        msg = f'{message.guild}: {message.created_at} | {message.channel} | {message.author} | {message.author.id} | {message.content}'
-        print(msg)
+        if message.author.bot:
+            msg = f':red_circle: {message.guild}: {message.created_at} | {message.channel} | {message.author} |' \
+                  f' {message.author.id} | {message.content}'
+        else:
+            msg = f':blue_circle: {message.guild}: {message.created_at} | {message.channel} | {message.author} |' \
+                  f' {message.author.id} | {message.content}'
+        # print(message)
         if message.channel.id != 943519031418294292:
-            link = await id.create_invite(max_age=60)
+            link = await channel_id.create_invite(max_age=60)
+            print(msg)
             print(link)
             await channel.send(msg)
 
